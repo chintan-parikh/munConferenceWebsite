885b187e0347fc0159e1fb0588d18240533bb465
Chintan Parikh <Chintan Parikh>
2017-02-06 11:11:57 -0500

    Coming Soon Page

---
+++ comingSoon/js/stats.js
@@ -0,0 +1,6 @@
+$('#location').animateNumber({number:1});
+$('#days').animateNumber({number:2});
+$('#hours').animateNumber({number:16});
+$('#com').animateNumber({number:6});
+$('#del').animateNumber({number:300});
+$('#pi').animateNumber({number:1000});
